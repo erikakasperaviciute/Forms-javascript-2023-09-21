70614343d52a3fcb9aaca0909adf25e70207fa4b
erikakasperaviciute <erika.kasperaviciute@hotmail.lt>
2023-09-25 13:18:47 +0300

    pridėta 6-ta užduotis

--- form-task.js
+++ form-task.js
@@ -21,13 +21,173 @@
 // 2. Paspaudus šį mygtuką, studento elementas yra ištrinamas.
 // 3. Ištrynus studentą, turi iššokti <span> elementas, kuris informuoja apie studento ištrynimą: „Studentas (Vardas Pavardė) sėkmingai ištrintas.". Šis span elementas dingsta po 5 sekundžių.
 
+// ŠEŠTA UŽDUOTIS:
+// 1. Sukurti pradinius duomenų masyvą, kuriame būtų bent 5 studentų duomenys (objektų formatu).
+// 2. Sukurti funkciją, kuri priima šiuos duomenis ir užkrovus puslapį į ekraną iškart išveda duomenis iš šio masyvo.
+
+const studentsArr = [
+  {
+    personName: "John",
+    personSurname: "Amstrong",
+    personAge: 32,
+    mobileNo: 862589123,
+    email: "john@email.com",
+    knowledge: 7,
+    personGroup: "FEU8",
+    personPreferences: ["PHP", "Node.js"],
+  },
+  {
+    personName: "Ted",
+    personSurname: "Baker",
+    personAge: 19,
+    mobileNo: 862589321,
+    email: "ted@email.com",
+    knowledge: 4,
+    personGroup: "FEU2",
+    personPreferences: ["JavaScript"],
+  },
+  {
+    personName: "Lilly",
+    personSurname: "Collins",
+    personAge: 24,
+    mobileNo: 862589555,
+    email: "lilly@email.com",
+    knowledge: 8,
+    personGroup: "FEU3",
+    personPreferences: ["Java"],
+  },
+  {
+    personName: "Sarah",
+    personSurname: "Miller",
+    personAge: 20,
+    mobileNo: 862589666,
+    email: "sarah@email.com",
+    knowledge: 3,
+    personGroup: "FEU8",
+    personPreferences: ["PHP", "JavaScript", "Node.js"],
+  },
+  {
+    personName: "Thomas",
+    personSurname: "Garcia",
+    personAge: 38,
+    mobileNo: 862589777,
+    email: "thomas@email.com",
+    knowledge: 9,
+    personGroup: "FEU10",
+    personPreferences: ["Python"],
+  },
+];
+
+// console.log(studentsArr);
+
+function getStudents() {
+  studentsArr.forEach((student) => {
+    const contactsForm = document.querySelector("#contacts-form");
+    let studentsList = document.querySelector("#students-list");
+    let studentItem = document.createElement("div");
+    studentItem.classList.add("student-item");
+    const personMainInfo = document.createElement("h3");
+    personMainInfo.textContent = `${student.personName} ${student.personSurname}, ${student.personAge} m.`;
+
+    let isHidden = true;
+
+    const personMobileNo = document.createElement("p");
+    personMobileNo.textContent = `Telefono Nr.: `;
+
+    const mobileNoHidden = document.createElement("span");
+    mobileNoHidden.textContent = "****";
+
+    personMobileNo.append(mobileNoHidden);
+
+    const personEmail = document.createElement("p");
+    personEmail.textContent = `El. pašto adresas: `;
+
+    const emailHidden = document.createElement("span");
+    emailHidden.textContent = "****";
+
+    personEmail.append(emailHidden);
+
+    const toggleContactsBtn = document.createElement("button");
+    toggleContactsBtn.textContent = "Rodyti asmens duomenis";
+
+    toggleContactsBtn.addEventListener("click", () => {
+      isHidden = !isHidden;
+      if (isHidden) {
+        toggleContactsBtn.textContent = "Rodyti asmens duomenis";
+        mobileNoHidden.textContent = "****";
+        emailHidden.textContent = "****";
+      } else {
+        toggleContactsBtn.textContent = "Slėpti asmens duomenis";
+        mobileNoHidden.textContent = student.mobileNo;
+        emailHidden.textContent = student.email;
+      }
+    });
+
+    const personKnowledgeValue = document.createElement("p");
+    personKnowledgeValue.textContent = `IT žinių vertinimas: ${student.knowledge}`;
+
+    const personGroupValue = document.createElement("p");
+    personGroupValue.textContent = `Priskirta grupė: ${student.personGroup}`;
+
+    const preferencesListTitle = document.createElement("p");
+    preferencesListTitle.textContent = `Studentą dominančios programavimo kalbos:`;
+    const personPreferencesList = document.createElement("ul");
+
+    preferencesListTitle.append(personPreferencesList);
+
+    if (student.personPreferences.length > 0) {
+      student.personPreferences.forEach((preference) => {
+        const personPreferencesListItem = document.createElement("li");
+        personPreferencesListItem.textContent = preference;
+        personPreferencesList.append(personPreferencesListItem);
+      });
+    } else {
+      const noPreference = document.createElement("li");
+      noPreference.textContent = "Nepasirinkta nei viena programavimo kalba";
+      personPreferencesList.append(noPreference);
+    }
+
+    function createMessage(message, textColor) {
+      const messageElement = document.createElement("span");
+      messageElement.textContent = message;
+      messageElement.style.color = textColor;
+      contactsForm.after(messageElement);
+      setTimeout(() => {
+        messageElement.remove();
+      }, 5000);
+    }
+
+    const deleteStudentBtn = document.createElement("button");
+    deleteStudentBtn.textContent = "Ištrinti studentą";
+
+    deleteStudentBtn.addEventListener("click", () => {
+      studentItem.remove();
+      const deletedStudentText = `Ištrintas studentas (${student.personName} ${student.personSurname})`;
+      createMessage(deletedStudentText, "red");
+    });
+
+    studentItem.append(
+      personMainInfo,
+      personMobileNo,
+      personEmail,
+      personKnowledgeValue,
+      personGroupValue,
+      preferencesListTitle,
+      toggleContactsBtn,
+      deleteStudentBtn
+    );
+    studentsList.prepend(studentItem);
+  });
+}
+
+getStudents();
+
 function init() {
   const contactsForm = document.querySelector("#contacts-form");
   let studentsList = document.querySelector("#students-list");
 
   contactsForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    // console.log(event);
 
     const form = event.target;
 
@@ -46,58 +206,39 @@ function init() {
     const personMainInfo = document.createElement("h3");
     personMainInfo.textContent = `${personName} ${personSurname}, ${personAge} m.`;
 
-    // const personMobileNo = document.createElement("p");
-    // personMobileNo.textContent = `Telefono Nr.: ****`;
-
-    // const personEmail = document.createElement("p");
-    // personEmail.textContent = `El. pašto adresas: ****`;
-
-    // const toggleContactsBtn = document.createElement("button");
-    // toggleContactsBtn.textContent = "Rodyti asmens duomenis";
-
-    // toggleContactsBtn.addEventListener("click", () => {
-    //   if (
-    //     personMobileNo.textContent === `Telefono Nr.: ****` &&
-    //     personEmail.textContent === `El. pašto adresas: ****`
-    //   ) {
-    //     personMobileNo.textContent = `Telefono Nr.: ${mobileNo}`;
-    //     personEmail.textContent = `El. pašto adresas: ${email}`;
-    //     toggleContactsBtn.textContent = "Slėpti asmens duomenis";
-    //   } else {
-    //     personMobileNo.textContent = `Telefono Nr.: ****`;
-    //     personEmail.textContent = `El. pašto adresas: ****`;
-    //     toggleContactsBtn.textContent = "Rodyti asmens duomenis";
-    //   }
-    // });
-    const hiddenText = document.createElement("span");
-    hiddenText.textContent = "****";
+    let isHidden = true;
 
     const personMobileNo = document.createElement("p");
-    personMobileNo.textContent = `Telefono Nr.: ${hiddenText.textContent}`;
+    personMobileNo.textContent = `Telefono Nr.: `;
+
+    const mobileNoHidden = document.createElement("span");
+    mobileNoHidden.textContent = "****";
+
+    personMobileNo.append(mobileNoHidden);
 
     const personEmail = document.createElement("p");
-    personEmail.textContent = `El. pašto adresas: ${hiddenText.textContent}`;
+    personEmail.textContent = `El. pašto adresas: `;
+
+    const emailHidden = document.createElement("span");
+    emailHidden.textContent = "****";
+
+    personEmail.append(emailHidden);
 
     const toggleContactsBtn = document.createElement("button");
     toggleContactsBtn.textContent = "Rodyti asmens duomenis";
 
     toggleContactsBtn.addEventListener("click", () => {
-      if (
-        personMobileNo.textContent.includes(hiddenText.textContent) &&
-        personEmail.textContent.includes(hiddenText.textContent)
-      ) {
-        personMobileNo.textContent = `Telefono Nr.: ${mobileNo}`;
-        personEmail.textContent = `El. pašto adresas: ${email}`;
-        toggleContactsBtn.textContent = "Slėpti asmens duomenis";
-      } else {
-        personMobileNo.textContent = `Telefono Nr.: ${hiddenText.textContent}`;
-        personEmail.textContent = `El. pašto adresas: ${hiddenText.textContent}`;
+      isHidden = !isHidden;
+      if (isHidden) {
         toggleContactsBtn.textContent = "Rodyti asmens duomenis";
+        mobileNoHidden.textContent = "****";
+        emailHidden.textContent = "****";
+      } else {
+        toggleContactsBtn.textContent = "Slėpti asmens duomenis";
+        mobileNoHidden.textContent = mobileNo;
+        emailHidden.textContent = email;
       }
     });
-
-    // const personContactInfo = document.createElement("p");
-    // personContactInfo.innerHTML = `Telefono Nr.: ${mobileNo} <br> El. pašto adresas: ${email}`;
 
     const personKnowledgeValue = document.createElement("p");
     personKnowledgeValue.textContent = `IT žinių vertinimas: ${knowledge}`;
@@ -122,31 +263,6 @@ function init() {
       noPreference.textContent = "Nepasirinkta nei viena programavimo kalba";
       personPreferencesList.append(noPreference);
     }
-
-    // const createdStudentElement = document.createElement("span");
-    // createdStudentElement.textContent = `Sukurtas studentas (${personName} ${personSurname})`;
-    // createdStudentElement.style.color = "green";
-    // contactsForm.after(createdStudentElement);
-
-    // setTimeout(() => {
-    //   createdStudentElement.remove();
-    // }, 5000);
-
-    // const deleteStudentBtn = document.createElement("button");
-    // deleteStudentBtn.textContent = "Ištrinti studentą";
-
-    // deleteStudentBtn.addEventListener("click", () => {
-    //   studentItem.remove();
-
-    //   const deletedStudentElement = document.createElement("span");
-    //   deletedStudentElement.textContent = `Ištrintas studentas (${personName} ${personSurname})`;
-    //   deletedStudentElement.style.color = "red";
-    //   contactsForm.after(deletedStudentElement);
-
-    //   setTimeout(() => {
-    //     deletedStudentElement.remove();
-    //   }, 5000);
-    // });
 
     function createMessage(message, textColor) {
       const messageElement = document.createElement("span");
